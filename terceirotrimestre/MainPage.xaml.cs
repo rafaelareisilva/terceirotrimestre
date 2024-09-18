fe73af1a33ddabc95765ce7485d5c046b67e1a52
rafaela <reiisrafaela@icloud.com>
2024-09-18 11:45:37 -0300

    18/09

--- terceirotrimestre/MainPage.xaml.cs
+++ terceirotrimestre/MainPage.xaml.cs
@@ -1,2 +1,17 @@
-﻿
+﻿namespace terceirotrimestre;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		
+	}
+}
 
