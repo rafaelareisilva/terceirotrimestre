54c90742f0aec025fba287f5ce23e232ae515285
rafaelareisilva <reiisrafaela@icloud.com>
2024-09-19 09:02:28 -0300

    19/09

---
+++ terceirotrimestre/Gerenciador.cs
@@ -0,0 +1,14 @@
+using terceirotrimestre;
+
+public class Gerenciador 
+{
+    List<Questao> ListaQuestoes=new List<Questao>();
+    List<int> ListaQuestoesRespondidas=new List<int>();
+    Questao QuestaoCorrente;
+    public Gerenciador (Label labelPerg, Button btnResp01, Button btnResp02, Button btnResp03, Button btnResp04, Button btnResp05)
+    {
+        CriaPerguntas(labelPerg, ButtonResposta1, ButtonResposta2, ButtonResposta3, ButtonResposta4, ButtonResposta5);
+    }
+
+    void CriaPerguntas(Label labelPerg, Button )
+}
